882156b039e7455ceaa94013ee81508cfbe2fa67
murageh <muragehdeveloper@gmail.com>
2024-04-03 21:17:28 +0300

    minor UI improvements
    Signed-off-by: murageh <muragehdeveloper@gmail.com>

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:utibu_health_app/main.dart';
+import 'package:utibu_health_app/components/inputs.dart';
 import 'package:utibu_health_app/pages/home/patient.dart';
 import 'package:utibu_health_app/pages/home/pharmacist.dart';
 import 'package:utibu_health_app/pages/register_page.dart';
@@ -25,9 +24,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
       body: Consumer<IdentityProvider>(
           builder: (context, identityProvider, child) {
         if (kDebugMode) {
@@ -43,90 +39,107 @@ class _LoginPageState extends State<LoginPage> {
         }
 
         return Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: emailController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your email";
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      hintText: "Email",
-                      contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      "Utibu Health",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your password";
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      hintText: "Password",
-                      contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                    const Text(
+                      "Welcome back. Please login to continue.",
                     ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: _isLoggingIn
-                        ? const Center(child: CircularProgressIndicator())
-                        : ElevatedButton(
-                            onPressed: () async {
-                              if (_formKey.currentState != null &&
-                                  _formKey.currentState!.validate()) {
-                                setState(() {
-                                  _isLoggingIn = true;
-                                });
-                                var user = await ApiService().login(
-                                  emailController.text,
-                                  passwordController.text,
-                                );
-                                setState(() {
-                                  _isLoggingIn = false;
-                                });
-                                if (user != null) {
-                                  if (kDebugMode) {
-                                    print(user);
-                                  }
-                                  identityProvider.updateIdentityUser(user);
-                                } else {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text("Invalid email or password"),
-                                      backgroundColor: Colors.red,
-                                      showCloseIcon: true,
-                                    ),
+                    const SizedBox(height: 16),
+                    RoundedTextField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your email";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Email",
+                        contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                      ),
+                    ),
+                    RoundedTextField(
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your password";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                        contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                      child: _isLoggingIn
+                          ? const Center(child: CircularProgressIndicator())
+                          : SubtlyRoundedElevatedButton(
+                              onPressed: () async {
+                                if (_formKey.currentState != null &&
+                                    _formKey.currentState!.validate()) {
+                                  setState(() {
+                                    _isLoggingIn = true;
+                                  });
+                                  var user = await ApiService().login(
+                                    emailController.text,
+                                    passwordController.text,
                                   );
+                                  setState(() {
+                                    _isLoggingIn = false;
+                                  });
+                                  if (user != null) {
+                                    if (kDebugMode) {
+                                      print(user);
+                                    }
+                                    identityProvider.updateIdentityUser(user);
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content:
+                                            Text("Invalid email or password"),
+                                        backgroundColor: Colors.red,
+                                        showCloseIcon: true,
+                                      ),
+                                    );
+                                  }
                                 }
-                              }
-                            },
-                            child: const Text("Login"),
+                              },
+                              child: const Text("Login"),
+                            ),
+                    ),
+                    const SizedBox(height: 8),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
                           ),
-                  ),
-                  const SizedBox(height: 8),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterPage(),
-                        ),
-                      );
-                    },
-                    child: const Text("Don't have an account? Register"),
-                  ),
-                ],
+                        );
+                      },
+                      child: const Text("Don't have an account? Register"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,5 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:utibu_health_app/components/inputs.dart';
 import 'package:utibu_health_app/pages/login_page.dart';
 
 import '../models/user.dart';
@@ -27,136 +26,147 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: firstNameController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your first name";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    "Utibu Health",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Thank you for choosing Utibu Health. Please register to continue.",
+                  ),
+                  const SizedBox(height: 16),
+                  RoundedTextField(
+                    controller: firstNameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your first name";
+                      }
+                      return null;
+                    },
                     hintText: "First Name",
-                    contentPadding: EdgeInsets.symmetric(vertical: 16.0),
                   ),
-                ),
-                TextFormField(
-                  controller: lastNameController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your last name";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
+                  RoundedTextField(
+                    controller: lastNameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your last name";
+                      }
+                      return null;
+                    },
                     hintText: "Last Name",
-                    contentPadding: EdgeInsets.symmetric(vertical: 16.0),
                   ),
-                ),
-                TextFormField(
-                  controller: usernameController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your username";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
+                  RoundedTextField(
+                    controller: usernameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your username";
+                      }
+                      return null;
+                    },
                     hintText: "Username",
-                    contentPadding: EdgeInsets.symmetric(vertical: 16.0),
                   ),
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your password";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
+                  RoundedTextField(
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      }
+                      return null;
+                    },
                     hintText: "Password",
-                    contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                TextFormField(
-                  controller: phoneNumberController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your phone number";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.phone,
-                  decoration: const InputDecoration(
+                  RoundedTextField(
+                    controller: phoneNumberController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your phone number";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.phone,
                     hintText: "Phone Number",
-                    contentPadding: EdgeInsets.symmetric(vertical: 16.0),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: _isRegistering
-                      ? const Center(child: CircularProgressIndicator())
-                      : ElevatedButton(
-                          onPressed: () async {
-                            if (_formKey.currentState != null &&
-                                _formKey.currentState!.validate()) {
-                              setState(() {
-                                _isRegistering = true;
-                              });
-                              bool registered = await ApiService().register(
-                                User(
-                                  null,
-                                  userId: 0,
-                                  username: usernameController.text,
-                                  password: passwordController.text,
-                                  firstName: firstNameController.text,
-                                  lastName: lastNameController.text,
-                                  phoneNumber: phoneNumberController.text,
-                                ),
-                              );
-                              setState(() {
-                                _isRegistering = false;
-                              });
-                              if (registered) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text(
-                                        "Registration successful, please login."),
-                                    backgroundColor: Colors.green,
-                                    showCloseIcon: true,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: _isRegistering
+                        ? const Center(child: CircularProgressIndicator())
+                        : SubtlyRoundedElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState != null &&
+                                  _formKey.currentState!.validate()) {
+                                setState(() {
+                                  _isRegistering = true;
+                                });
+                                bool registered = await ApiService().register(
+                                  User(
+                                    null,
+                                    userId: 0,
+                                    username: usernameController.text,
+                                    password: passwordController.text,
+                                    firstName: firstNameController.text,
+                                    lastName: lastNameController.text,
+                                    phoneNumber: phoneNumberController.text,
                                   ),
                                 );
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => const LoginPage()));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text("Registration failed"),
-                                    backgroundColor: Colors.red,
-                                    showCloseIcon: true,
-                                  ),
-                                );
+                                setState(() {
+                                  _isRegistering = false;
+                                });
+                                if (registered) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                          "Registration successful, please login."),
+                                      backgroundColor: Colors.green,
+                                      showCloseIcon: true,
+                                    ),
+                                  );
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const LoginPage()));
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text("Registration failed"),
+                                      backgroundColor: Colors.red,
+                                      showCloseIcon: true,
+                                    ),
+                                  );
+                                }
                               }
-                            }
-                          },
-                          child: const Text("Register"),
+                            },
+                            child: const Text("Register"),
+                          ),
+                  ),
+                  const SizedBox(height: 0.0),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
                         ),
-                ),
-              ],
+                      );
+                    },
+                    child: const Text("Already have an account? Login"),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
